f7678f1a5b8060083a0b8f0cb0ca433b8cd6ef71
Leonid <chakalidi@greensight.ru>
2024-09-03 00:46:10 +0300

    add favicon

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"><link rel="icon" href="/images/favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ресторан «Камелот» в бизнес-отеле «Премьер», в центре Краснодара</title>
     <meta name="description"
