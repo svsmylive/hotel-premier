5ff03ed0f5f71329d7f2cf491b00c45d994eb207
Leonid <chakalidi@greensight.ru>
2024-09-05 21:44:16 +0300

    meta viewport

--- resources/views/park-krasnodar.blade.php
+++ resources/views/park-krasnodar.blade.php
@@ -2,6 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8"><link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
     <title>Отель рядом с парком Краснодар - бизнес-отель «Премьер»</title>
     <meta name="description"
