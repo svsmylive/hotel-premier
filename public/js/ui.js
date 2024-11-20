6a82a6a3bd60329541ebea37842f4bb9774c4739
Leonid <chakalidi@greensight.ru>
2024-11-20 20:55:38 +0300

    доработки 20.11.2024

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,16 +1,19 @@
 const initSelectOptions = () => {
-  const selectOption = document.querySelector('.select-option');
-  const selectedItem = selectOption.querySelector('.select-option-selected');
-  selectedItem.addEventListener('click', () => {
-    selectOption.classList.toggle('active');
-  });
-  const variants = selectOption.querySelectorAll('.select-option-variants > div');
-  variants.forEach(variant => {
-    variant.addEventListener('click', () => {
+  const selectOptions = document.querySelectorAll('.select-option');
+
+  selectOptions.forEach(selectOption => {
+    const selectedItem = selectOption.querySelector('.select-option-selected');
+    selectedItem.addEventListener('click', () => {
       selectOption.classList.toggle('active');
-      selectedItem.querySelector('.select-option-selected__text').textContent = variant.textContent;
-      variants.forEach(_variant => _variant.classList.remove('active'));
-      variant.classList.add('active');
+    });
+    const variants = selectOption.querySelectorAll('.select-option-variants > div');
+    variants.forEach(variant => {
+      variant.addEventListener('click', () => {
+        selectOption.classList.toggle('active');
+        selectedItem.querySelector('.select-option-selected__text').textContent = variant.textContent;
+        variants.forEach(_variant => _variant.classList.remove('active'));
+        variant.classList.add('active');
+      });
     });
   });
 };
