a0bb949a96514e82d7f9a7517e92360a98b4dbee
Leonid <chakalidi@greensight.ru>
2024-11-27 23:01:45 +0300

    доработки 27.11.2024 fix video Microsoft Edge

--- public/js/sections/intro.js
+++ public/js/sections/intro.js
@@ -1,16 +1,20 @@
 const introVideoContainer = document.querySelector('.intro-section__video');
 const video = document.createElement('video');
 
-video.addEventListener('loadeddata', () => {
-  introVideoContainer.appendChild(video);
-  introVideoContainer.classList.remove('loading');
-  video.play();
-});
-
+// Установка атрибутов видео до загрузки
 video.src = '/assets/videos/intro-video-small.mp4';
 video.loop = true;
-video.autoplay = true;
-video.muted = true;
-video.playsInline = true;
+video.muted = true; // Убедитесь, что видео без звука
+video.autoplay = true; // Автозапуск
+video.playsInline = true; // Поддержка для мобильных устройств
+
+// После загрузки данных видео, добавьте его в DOM
+video.addEventListener('loadeddata', () => {
+    introVideoContainer.appendChild(video);
+    introVideoContainer.classList.remove('loading');
+    video.play().catch(err => {
+        console.error('Автоматическое воспроизведение не удалось:', err);
+    });
+});
 
 video.load();
