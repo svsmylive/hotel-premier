b18f5ddd9731f88ccc5d2308f74f9e6d2ec0fc4f
Leonid <chakalidi@greensight.ru>
2025-02-13 20:44:21 +0300

    не кешируемый js

--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,8 @@ import './sections/header.js';
 
 import './sections/intro.js';
 import './sections/numbers.js';
-import './sections/gastronomya.js';
-import './sections/events.js';
+import './sections/gastronomya1.js';
+import './sections/events1.js';
 import './sections/wellness.js';
 
 import './sections/footer.js';
