6731e86b6bf822e55145737925401bc5d6d88c20
Leonid <chakalidi@greensight.ru>
2025-08-06 00:25:09 +0300

    модальное окно со скидкой

--- public/js/promokod-modal.js
+++ public/js/promokod-modal.js
@@ -1,4 +1,4 @@
 import {closeModal, openModal} from './modals.js';
 
 window.addEventListener('load', () => openModal('modal-promokod'));
-setTimeout(() => closeModal('modal-promokod'), 8000);
+setTimeout(() => closeModal('modal-promokod'), 10000);
