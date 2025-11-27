d0e027e507ac93f0fa3bf994bc4bc4628dc52797
Leonid <chakalidi@greensight.ru>
2025-11-27 15:20:46 +0300

    fix

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -581,39 +581,39 @@ Arial, Helvetica, sans-serif; position: absolute; right: 0; bottom: 0; font-size
     import '/js/cookies.js';
     import '/js/promokod-modal.js';
 </script>
-<!-- calltouch -->
-<script>
-    (function (w, d, n, c) {
-        w.CalltouchDataObject = n;
-        w[n] = function () {
-            w[n]["callbacks"].push(arguments)
-        };
-        if (!w[n]["callbacks"]) {
-            w[n]["callbacks"] = []
-        }
-        w[n]["loaded"] = false;
-        if (typeof c !== "object") {
-            c = [c]
-        }
-        w[n]["counters"] = c;
-        for (var i = 0; i < c.length; i += 1) {
-            p(c[i])
-        }
+{{--<!-- calltouch -->--}}
+{{--<script>--}}
+{{--    (function (w, d, n, c) {--}}
+{{--        w.CalltouchDataObject = n;--}}
+{{--        w[n] = function () {--}}
+{{--            w[n]["callbacks"].push(arguments)--}}
+{{--        };--}}
+{{--        if (!w[n]["callbacks"]) {--}}
+{{--            w[n]["callbacks"] = []--}}
+{{--        }--}}
+{{--        w[n]["loaded"] = false;--}}
+{{--        if (typeof c !== "object") {--}}
+{{--            c = [c]--}}
+{{--        }--}}
+{{--        w[n]["counters"] = c;--}}
+{{--        for (var i = 0; i < c.length; i += 1) {--}}
+{{--            p(c[i])--}}
+{{--        }--}}
 
-        function p(cId) {
-            var a = d.getElementsByTagName("script")[0], s = d.createElement("script"), i = function () {
-                a.parentNode.insertBefore(s, a)
-            }, m = typeof Array.prototype.find === 'function', n = m ? "init-min.js" : "init.js";
-            s.async = true;
-            s.src = "https://mod.calltouch.ru/" + n + "?id=" + cId;
-            if (w.opera == "[object Opera]") {
-                d.addEventListener("DOMContentLoaded", i, false)
-            } else {
-                i()
-            }
-        }
-    })(window, document, "ct", "d0bjiqi8");
-</script>
+{{--        function p(cId) {--}}
+{{--            var a = d.getElementsByTagName("script")[0], s = d.createElement("script"), i = function () {--}}
+{{--                a.parentNode.insertBefore(s, a)--}}
+{{--            }, m = typeof Array.prototype.find === 'function', n = m ? "init-min.js" : "init.js";--}}
+{{--            s.async = true;--}}
+{{--            s.src = "https://mod.calltouch.ru/" + n + "?id=" + cId;--}}
+{{--            if (w.opera == "[object Opera]") {--}}
+{{--                d.addEventListener("DOMContentLoaded", i, false)--}}
+{{--            } else {--}}
+{{--                i()--}}
+{{--            }--}}
+{{--        }--}}
+{{--    })(window, document, "ct", "d0bjiqi8");--}}
+{{--</script>--}}
 
 <script src="//widget.reservationsteps.ru/js/bnovo.js"></script>
 <script src="https://widget.reservationsteps.ru/iframe/library/dist/booking_iframe.js"></script>
