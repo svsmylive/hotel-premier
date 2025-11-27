890cc9a05085bc6f5227bf631376b818aab6dd1d
Leonid <chakalidi@greensight.ru>
2025-11-28 00:51:02 +0300

    fix

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -23,28 +23,28 @@
     <link rel="icon" type="image/png" href="./favicon.png"/>
 
     <!-- Yandex.Metrika counter -->
-{{--    <script type="text/javascript">--}}
-{{--        (function (m, e, t, r, i, k, a) {--}}
-{{--            m[i] = m[i] || function () {--}}
-{{--                (m[i].a = m[i].a || []).push(arguments)--}}
-{{--            };--}}
-{{--            m[i].l = 1 * new Date();--}}
-{{--            for (var j = 0; j < document.scripts.length; j++) {--}}
-{{--                if (document.scripts[j].src === r) {--}}
-{{--                    return;--}}
-{{--                }--}}
-{{--            }--}}
-{{--            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)--}}
-{{--        })--}}
-{{--        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");--}}
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-{{--        ym(99236087, "init", {--}}
-{{--            clickmap: true,--}}
-{{--            trackLinks: true,--}}
-{{--            accurateTrackBounce: true,--}}
-{{--            webvisor: true--}}
-{{--        });--}}
-{{--    </script>--}}
+        ym(99236087, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
     <noscript>
         <div><img src="https://mc.yandex.ru/watch/99236087" style="position:absolute; left:-9999px;" alt=""/></div>
     </noscript>
@@ -573,47 +573,47 @@ Arial, Helvetica, sans-serif; position: absolute; right: 0; bottom: 0; font-size
     <button class="modal-cookie__btn-ok btn-primary">Хорошо</button>
 </div>
 
-{{--<script src="/js/lib/imask.js"></script>--}}
-{{--<script type="module">--}}
-{{--    import '/js/ui.js';--}}
-{{--    import '/js/sections/header.js';--}}
-{{--    import '/js/sections/footer.js';--}}
-{{--    import '/js/cookies.js';--}}
-{{--    import '/js/promokod-modal.js';--}}
-{{--</script>--}}
-{{--<!-- calltouch -->--}}
-{{--<script>--}}
-{{--    (function (w, d, n, c) {--}}
-{{--        w.CalltouchDataObject = n;--}}
-{{--        w[n] = function () {--}}
-{{--            w[n]["callbacks"].push(arguments)--}}
-{{--        };--}}
-{{--        if (!w[n]["callbacks"]) {--}}
-{{--            w[n]["callbacks"] = []--}}
-{{--        }--}}
-{{--        w[n]["loaded"] = false;--}}
-{{--        if (typeof c !== "object") {--}}
-{{--            c = [c]--}}
-{{--        }--}}
-{{--        w[n]["counters"] = c;--}}
-{{--        for (var i = 0; i < c.length; i += 1) {--}}
-{{--            p(c[i])--}}
-{{--        }--}}
+<script src="/js/lib/imask.js"></script>
+<script type="module">
+    import '/js/ui.js';
+    import '/js/sections/header.js';
+    import '/js/sections/footer.js';
+    import '/js/cookies.js';
+    import '/js/promokod-modal.js';
+</script>
+<!-- calltouch -->
+<script>
+    (function (w, d, n, c) {
+        w.CalltouchDataObject = n;
+        w[n] = function () {
+            w[n]["callbacks"].push(arguments)
+        };
+        if (!w[n]["callbacks"]) {
+            w[n]["callbacks"] = []
+        }
+        w[n]["loaded"] = false;
+        if (typeof c !== "object") {
+            c = [c]
+        }
+        w[n]["counters"] = c;
+        for (var i = 0; i < c.length; i += 1) {
+            p(c[i])
+        }
 
-{{--        function p(cId) {--}}
-{{--            var a = d.getElementsByTagName("script")[0], s = d.createElement("script"), i = function () {--}}
-{{--                a.parentNode.insertBefore(s, a)--}}
-{{--            }, m = typeof Array.prototype.find === 'function', n = m ? "init-min.js" : "init.js";--}}
-{{--            s.async = true;--}}
-{{--            s.src = "https://mod.calltouch.ru/" + n + "?id=" + cId;--}}
-{{--            if (w.opera == "[object Opera]") {--}}
-{{--                d.addEventListener("DOMContentLoaded", i, false)--}}
-{{--            } else {--}}
-{{--                i()--}}
-{{--            }--}}
-{{--        }--}}
-{{--    })(window, document, "ct", "d0bjiqi8");--}}
-{{--</script>--}}
+        function p(cId) {
+            var a = d.getElementsByTagName("script")[0], s = d.createElement("script"), i = function () {
+                a.parentNode.insertBefore(s, a)
+            }, m = typeof Array.prototype.find === 'function', n = m ? "init-min.js" : "init.js";
+            s.async = true;
+            s.src = "https://mod.calltouch.ru/" + n + "?id=" + cId;
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", i, false)
+            } else {
+                i()
+            }
+        }
+    })(window, document, "ct", "d0bjiqi8");
+</script>
 
 <script src="//widget.reservationsteps.ru/js/bnovo.js"></script>
 <script src="https://widget.reservationsteps.ru/iframe/library/dist/booking_iframe.js"></script>
